455382e72fb4c1da5a3a5ab98e0798d83b58da2e
Fernando Cabrera <diego.cabrera@kushkipagos.com>
2022-05-28 18:30:24 -0500

    Manage observable fromEvent

---
+++ src/observables/06-fromEvent.ts
@@ -0,0 +1,25 @@
+import {fromEvent} from 'rxjs';
+
+
+const src1$ = fromEvent<PointerEvent>(document, 'click')
+const src2$ = fromEvent<KeyboardEvent>(document, 'keyup')
+
+const observer = {
+    next: val => console.log(val)
+}
+
+// src1$.subscribe(event =>{
+//     console.log(event.x);
+//     console.log(event.y);
+    
+// });
+
+src1$.subscribe(({x,y}) => {
+    console.log(x);
+    console.log(y);
+    
+})
+
+src2$.subscribe(event =>{
+    console.log(event.key);
+});
